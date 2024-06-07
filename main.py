cba49c94d715302cb2df9afdf3c41cc0ffe49a8f
Afifi40 <ahmed.a4046@gmail.com>
2024-06-07 13:05:13 +0200

    Added a stub for input data including resources and Menu, printed them both to make sure everything is working fine

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import data_stub
 
 
-def print_hi(name):
+def print_input_data_stub(data):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Input Menu:  {data.MENU}')
+    print(f'Input Resources {data.resources}')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Afifi')
+    print_input_data_stub(data_stub)
+    print("END")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
