d22a2d0c19cd37b25cb33cc286254303fff2839f
Afifi40 <ahmed.a4046@gmail.com>
2024-06-09 10:19:56 +0200

    print report requirement have been achieved with a testing driver at main function

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
 import data_stub
+data = data_stub
 
 
-def print_input_data_stub(data):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Input Menu:  {data.MENU}')
+def print_report():
     print(f'Input Resources {data.resources}')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_input_data_stub(data_stub)
+    print_report()
     print("END")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
