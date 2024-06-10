dc52650808af93c67bbdbc805e2e9bb8d85b4d48
Afifi40 <ahmed.a4046@gmail.com>
2024-06-10 22:38:39 +0300

    Fixed a bug in driver code Resources were subtracted even if not enough money inserted (drink not served)

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ if __name__ == '__main__':
     # print(list(data_stub.MENU.keys()))
     while True:
         chosen_drink = my_coffee_machine.prompt_user()
-        is_enough_resources = my_coffee_machine.resources_check(chosen_drink)
-        if is_enough_resources:
+        if my_coffee_machine.resources_check(chosen_drink):
             coins = CoffeeMachine.enter_coins()
-            is_enough_money = my_coffee_machine.process_transaction(coins, chosen_drink)
-            # drink_cost: float
-            my_coffee_machine.make_coffee(chosen_drink)
+            if my_coffee_machine.process_transaction(coins, chosen_drink):
+                my_coffee_machine.make_coffee(chosen_drink)
     # print("Program end")
